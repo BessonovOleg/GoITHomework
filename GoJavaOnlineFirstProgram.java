2288ab59ccdca6fd0022e203db2d842d149c3c29
Oleg <oleg_en@ukr.net>
2016-01-28 13:20:57 +0200

    test commit2

--- GoJavaOnlineFirstProgram.java
+++ GoJavaOnlineFirstProgram.java
@@ -1,7 +1,6 @@
 public class GoJavaOnlineFirstProgram {
 	public static void main (String[] args) {
 		System.out.println("Welcome to Jaca Online Course!");
-		System.out.println("Test for GIT");
 		System.out.println("Change 2 for test GIT");
 	}
 }
