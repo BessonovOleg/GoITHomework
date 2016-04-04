16302425ed3b94aa1a5a686e55f8da602dfd72e5
oleg <oleg_en@ukr.net>
2016-04-04 23:00:43 +0300

    home work 3.1 final

--- HomeWorkEE31/src/Adjuster.java
+++ HomeWorkEE31/src/Adjuster.java
@@ -1,68 +1,58 @@
 
 public class Adjuster implements Semaphore {
     private int countPermits;
-    private int availablePermits;
-    private final Object lock = new Object();
+    private int maxPermits;
 
     public Adjuster(int permits) {
         countPermits = permits;
-        availablePermits = permits;
+        maxPermits = permits;
     }
 
     @Override
-    public void acquire() {
+    public synchronized void acquire() {
         // Запрашивает разрешение. Если есть свободное захватывает его.
         // Если нет - приостанавливает поток до тех пор пока не появится свободное разрешение.
         while (true) {
-             synchronized (lock) {
                 if (getAvailablePermits() > 0) {
-                    availablePermits--;
+                    maxPermits--;
                     break;
                 } else {
                     try {
-                        lock.wait();
+                        this.wait();
                     }catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                }
             }
         }
     }
 
-
     @Override
-    public void acquire(int permits) {
-        synchronized (lock) {
+    public synchronized void acquire(int permits) {
             for (int i = 0; i < permits; i++) {
                 acquire();
             }
-        }
     }
 
     // Отпускает разрешение возвращая его семафору.
     @Override
-    public void release() {
-        synchronized (lock) {
-            if (availablePermits < countPermits) {
-                availablePermits++;
-                lock.notify();
+    public synchronized void release() {
+            if (maxPermits < countPermits) {
+                maxPermits++;
+                this.notify();
             }
-        }
     }
 
     // Отпускает переданое количество разрешений возварщая их семафору.
     @Override
-    public void release(int permits) {
-        synchronized (lock) {
+    public synchronized void release(int permits) {
             for (int i = 0; i < permits; i++) {
                 release();
             }
-        }
     }
 
     // Возвращает количество свободных разрешений на данный момент.
     @Override
-    public int getAvailablePermits() {
-        return availablePermits;
+    public synchronized int getAvailablePermits() {
+        return maxPermits;
     }
 }
