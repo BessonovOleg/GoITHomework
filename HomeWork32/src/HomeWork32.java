190819b31c281fedb24b436a9a4ebd444419395b
oleg <oleg_en@ukr.net>
2016-04-14 20:07:04 +0300

    home work 32

---
+++ HomeWork32/src/HomeWork32.java
@@ -0,0 +1,97 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Phaser;
+
+public class HomeWork32 implements SquareSum{
+
+
+    public static void main(String[] args) {
+        HomeWork32 hw = new HomeWork32();
+        List<int[]> list = new ArrayList<>();
+        int[] ar = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+        System.out.println(hw.getSquareSum(ar,4));
+    }
+
+
+    @Override
+    public long getSquareSum(int[] values, int numberOfThreads) {
+        long result = 0;
+        List<int[]> list = new ArrayList<>();
+        list = separationArray(values,numberOfThreads);
+
+        List<Long> listResult = new ArrayList<>();
+        Phaser phaser = new Phaser();
+        Executor executor = Executors.newFixedThreadPool(numberOfThreads);
+        for(int i = 0;i < numberOfThreads; i++) {
+            executor.execute(new CalculatorSquareInArray(listResult,phaser,list.get(i)));
+        }
+
+        phaser.arriveAndAwaitAdvance();
+
+        for(long l:listResult) {
+            result += l;
+        }
+        phaser.arriveAndDeregister();
+
+        return result;
+    }
+
+
+    public class CalculatorSquareInArray implements Runnable {
+
+        private List<Long> res;
+        private Phaser p;
+        private int[] ar;
+        private long tmpRes = 0;
+
+        public CalculatorSquareInArray(List<Long> res,Phaser p, int[] ar) {
+            this.res = res;
+            this.p = p;
+            this.ar = ar;
+            p.register();
+        }
+
+        @Override
+        public void run() {
+            for(long l:ar) {
+                tmpRes += l*l;
+            }
+            res.add(tmpRes);
+            p.arriveAndDeregister();
+        }
+    }
+
+
+
+    public List<int[]> separationArray(int[] array, int count) {
+        List<int[]> result = new ArrayList<>();
+        int indexFrom = 0;
+        int indexTo = 0;
+        int countElements = 0;
+
+        if (array.length > 0 && count > 0) {
+            countElements = array.length / count;
+        } else {
+            return result;
+        }
+
+        if (array.length < count) {
+            result.add(array);
+        } else {
+            for (int i = 0; i < count; i++) {
+                if ( (indexFrom + countElements) > array.length || i == (count - 1) ) {
+                    indexTo = array.length;
+                } else {
+                    indexTo = indexFrom + countElements;
+                }
+                result.add(Arrays.copyOfRange(array, indexFrom, indexTo));
+                indexFrom = indexTo;
+            }
+        }
+        return result;
+    }
+
+}
---
+++ HomeWork32/src/SquareSum.java
@@ -0,0 +1,6 @@
+/**
+ * Created by user on 14.04.2016.
+ */
+public interface SquareSum {
+    long getSquareSum(int[] values, int numberOfThreads);
+}
