32fcb1f2ccb40763ecdac7bc974242ae9b9a6f43
oleg <oleg_en@ukr.net>
2016-06-13 22:25:40 +0300

    home work 5.2 continue 2

--- HomeWorkEE511/src/main/java/ua/goit/homework511/LogWriter.java
+++ HomeWorkEE511/src/main/java/ua/goit/homework511/LogWriter.java
@@ -4,7 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LogWriter {
-    final Logger logger = LoggerFactory.getLogger(LogWriter.class);
+    //final Logger logger = LoggerFactory.getLogger(LogWriter.class);
+    final Logger logger = LoggerFactory.getLogger("LogWriter");
 
     public LogWriter(String msgLog) {
         logger.debug(msgLog);
