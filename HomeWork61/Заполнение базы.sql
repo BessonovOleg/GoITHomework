ee5dffef546a0d1c876dbcd907f7ea0dcb4cbfac
oleg <oleg_en@ukr.net>
2016-06-26 14:21:59 +0300

    Home work 6.1

---
+++ HomeWork61/Заполнение базы.sql
@@ -0,0 +1,217 @@
+﻿--1) Заполняем сотрудников
+
+insert into "Workers"("LAST_NAME","FIRST_NAME","DOB","PHONE","POSITION","SALARY")
+select 'Иванов','Иван','1990-10-10','+380501234567','официант',4000;
+
+insert into "Workers"("LAST_NAME","FIRST_NAME","DOB","PHONE","POSITION","SALARY")
+select 'Газаашвили','Гога','1958-04-03','+380952223344','повар',9000;
+
+insert into "Workers"("LAST_NAME","FIRST_NAME","DOB","PHONE","POSITION","SALARY")
+select 'Маисеев','Изя','1965-01-01','+380504445512','менеджер',12000;
+
+insert into "Workers"("LAST_NAME","FIRST_NAME","DOB","PHONE","POSITION","SALARY")
+select 'Дубина','Николай','1980-11-11','+380504567890','охрана',3000;
+
+insert into "Workers"("LAST_NAME","FIRST_NAME","DOB","PHONE","POSITION","SALARY")
+select 'Сверепов','Алексей','1989-04-03','+380502222222','бармен',5000;
+
+insert into "Workers"("LAST_NAME","FIRST_NAME","DOB","PHONE","POSITION","SALARY")
+select 'Вяткина','Алла','1987-05-05','+380503333333','официант',4000;
+
+
+--2)Заполняем ингредиенты
+insert into "Ingredients"("ING_NAME")
+select 'майонез';
+
+insert into "Ingredients"("ING_NAME")
+select 'крабовые палочки';
+
+insert into "Ingredients"("ING_NAME")
+select 'картошка';
+
+insert into "Ingredients"("ING_NAME")
+select 'мясо';
+
+insert into "Ingredients"("ING_NAME")
+select 'буряк';
+
+insert into "Ingredients"("ING_NAME")
+select 'капуста';
+
+insert into "Ingredients"("ING_NAME")
+select 'молоко';
+
+insert into "Ingredients"("ING_NAME")
+select 'сироп';
+
+
+--3) Заполняем остатки ингредиентов на складе
+insert into "Warehouse"("ING_ID","QTY")
+select 1,150;
+
+insert into "Warehouse"("ING_ID","QTY")
+select 2,50;
+
+insert into "Warehouse"("ING_ID","QTY")
+select 3,80;
+
+insert into "Warehouse"("ING_ID","QTY")
+select 4,420;
+
+insert into "Warehouse"("ING_ID","QTY")
+select 5,512;
+
+insert into "Warehouse"("ING_ID","QTY")
+select 6,15;
+
+insert into "Warehouse"("ING_ID","QTY")
+select 7,96;
+
+insert into "Warehouse"("ING_ID","QTY")
+select 8,241;
+
+
+--4) Заполним категории блюд
+insert into "Category"("CAT_NAME")
+select 'Салаты';
+
+insert into "Category"("CAT_NAME")
+select 'Первые блюда';
+
+insert into "Category"("CAT_NAME")
+select 'Вторые блюда';
+
+insert into "Category"("CAT_NAME")
+select 'Коктейли';
+
+
+--5) Заполняем блюда
+insert into "Dishs"("DISH_NAME","CAT_ID","PRICE","WEIGHT")
+select 'Салат крабовый',1,15,150;
+
+insert into "Dishs"("DISH_NAME","CAT_ID","PRICE","WEIGHT")
+select 'Борщ',2,25,250;
+
+insert into "Dishs"("DISH_NAME","CAT_ID","PRICE","WEIGHT")
+select 'Картошка фри',3,10,150;
+
+insert into "Dishs"("DISH_NAME","CAT_ID","PRICE","WEIGHT")
+select 'Молочный коктейль',4,12,250;
+
+--6)Заполняем рецептуру
+--крабовый салат
+insert into "Recipes"("DISH_ID","ING_ID","QTY")
+select 1,1,20;
+
+insert into "Recipes"("DISH_ID","ING_ID","QTY")
+select 1,2,120;
+--борщ
+insert into "Recipes"("DISH_ID","ING_ID","QTY")
+select 2,3,50;
+
+insert into "Recipes"("DISH_ID","ING_ID","QTY")
+select 2,4,100;
+
+insert into "Recipes"("DISH_ID","ING_ID","QTY")
+select 2,5,50;
+
+insert into "Recipes"("DISH_ID","ING_ID","QTY")
+select 2,6,50;
+
+--картошка фри
+insert into "Recipes"("DISH_ID","ING_ID","QTY")
+select 3,3,150;
+
+--молочный коктейль
+insert into "Recipes"("DISH_ID","ING_ID","QTY")
+select 4,7,200;
+insert into "Recipes"("DISH_ID","ING_ID","QTY")
+select 4,8,50;
+
+--7) Зполняем меню
+insert into "Menu"("MENU_NAME")
+select 'Первые блюда';
+
+insert into "Menu"("MENU_NAME")
+select 'Вторые блюда';
+
+insert into "Menu"("MENU_NAME")
+select 'Комплексные обеды';
+
+--8) Заполняем состав блюд
+insert into "Menu_contents"("MENU_ID","DISH_ID")
+select 1,2;
+
+insert into "Menu_contents"("MENU_ID","DISH_ID")
+select 2,3;
+
+insert into "Menu_contents"("MENU_ID","DISH_ID")
+select 3,1;
+
+insert into "Menu_contents"("MENU_ID","DISH_ID")
+select 3,2;
+
+insert into "Menu_contents"("MENU_ID","DISH_ID")
+select 3,3;
+
+insert into "Menu_contents"("MENU_ID","DISH_ID")
+select 3,4;
+
+
+--9) Заполняем заказы
+insert into "Orders"("WORK_ID","TABLENO","ORDER_DATE")
+select 1,'1','2016-06-05';
+
+insert into "Orders"("WORK_ID","TABLENO","ORDER_DATE")
+select 6,'4','2016-06-07';
+
+--10) заполняем содержимое заказов
+--борщ
+insert into "Order_contents"("ORD_ID","DISH_ID","QTY")
+select 1,2,1;
+
+---комплексный обед
+insert into "Order_contents"("ORD_ID","DISH_ID","QTY")
+select 2,1,2;
+
+insert into "Order_contents"("ORD_ID","DISH_ID","QTY")
+select 2,2,1;
+
+insert into "Order_contents"("ORD_ID","DISH_ID","QTY")
+select 2,3,1;
+
+insert into "Order_contents"("ORD_ID","DISH_ID","QTY")
+select 2,4,1;
+
+
+--11) заполняем приготовленные блюда
+insert into "Cooked_dishs"("DISH_ID","WORK_ID","ORD_ID","DATE")
+select 2,2,1,'2016-06-05';
+
+
+
+insert into "Cooked_dishs"("DISH_ID","WORK_ID","ORD_ID","DATE")
+select 1,2,2,'2016-06-07';
+
+insert into "Cooked_dishs"("DISH_ID","WORK_ID","ORD_ID","DATE")
+select 2,2,2,'2016-06-07';
+
+insert into "Cooked_dishs"("DISH_ID","WORK_ID","ORD_ID","DATE")
+select 3,2,2,'2016-06-07';
+
+insert into "Cooked_dishs"("DISH_ID","WORK_ID","ORD_ID","DATE")
+select 4,2,2,'2016-06-07';
+
+
+
+
+--select * from "Workers"
+--select * from "Ingredients"
+--select * from "Warehouse"
+--select * from "Dishs"
+--select * from "Menu"
+--select * from "Menu_contents"
+--select * from "Recipes"
+--select * from "Orders"
+--select * from "Order_contents"
+--select * from "Cooked_dishs"
---
+++ HomeWork61/Создание базы.sql
@@ -0,0 +1,367 @@
+﻿--Создание базы данных
+CREATE DATABASE "Restaurant"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Russian_Russia.1251'
+       LC_CTYPE = 'Russian_Russia.1251'
+       CONNECTION LIMIT = -1;
+
+
+--Сотрудники
+CREATE TABLE "Workers"
+(
+  "WORK_ID" serial NOT NULL,
+  "LAST_NAME" character varying(50), -- Фамилия
+  "FIRST_NAME" character varying(50), -- Имя
+  "DOB" date, -- Дата рождения
+  "PHONE" character varying(50), -- Телефон
+  "POSITION" character varying, -- Должность
+  "SALARY" money, -- Оклад
+  CONSTRAINT "Workers_pkey" PRIMARY KEY ("WORK_ID")
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Workers"
+  OWNER TO postgres;
+COMMENT ON TABLE "Workers"
+  IS 'Сотрудники';
+COMMENT ON COLUMN "Workers"."LAST_NAME" IS 'Фамилия';
+COMMENT ON COLUMN "Workers"."FIRST_NAME" IS 'Имя';
+COMMENT ON COLUMN "Workers"."DOB" IS 'Дата рождения';
+COMMENT ON COLUMN "Workers"."PHONE" IS 'Телефон';
+COMMENT ON COLUMN "Workers"."POSITION" IS 'Должность';
+COMMENT ON COLUMN "Workers"."SALARY" IS 'Оклад';
+
+--Категории
+CREATE TABLE "Category"
+(
+  "CAT_ID" serial NOT NULL,
+  "CAT_NAME" character varying(255),
+  CONSTRAINT "Category_pkey" PRIMARY KEY ("CAT_ID")
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Category"
+  OWNER TO postgres;
+COMMENT ON TABLE "Category"
+  IS 'Категория блюд';
+
+--Меню
+CREATE TABLE "Menu"
+(
+  "MENU_ID" serial NOT NULL,
+  "MENU_NAME" character varying(255),
+  CONSTRAINT "Menu_pkey" PRIMARY KEY ("MENU_ID")
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Menu"
+  OWNER TO postgres;
+  
+--Ингредиенты
+CREATE TABLE "Ingredients"
+(
+  "ING_ID" serial NOT NULL,
+  "ING_NAME" character varying(255),
+  CONSTRAINT "Ingredients_pkey" PRIMARY KEY ("ING_ID")
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Ingredients"
+  OWNER TO postgres;
+COMMENT ON TABLE "Ingredients"
+  IS 'Ингредиенты';
+
+--Блюда
+CREATE TABLE "Dishs"
+(
+  "DISH_ID" serial NOT NULL,
+  "DISH_NAME" character varying(255),
+  "CAT_ID" integer, -- Категория
+  "PRICE" money,
+  "WEIGHT" real,
+  CONSTRAINT "Dishs_pkey" PRIMARY KEY ("DISH_ID"),
+  CONSTRAINT "Dish_to_category" FOREIGN KEY ("CAT_ID")
+      REFERENCES "Category" ("CAT_ID") MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Dishs"
+  OWNER TO postgres;
+COMMENT ON TABLE "Dishs"
+  IS 'Блюда';
+COMMENT ON COLUMN "Dishs"."CAT_ID" IS 'Категория';
+
+
+-- Index: "fki_Dish_to_category"
+
+-- DROP INDEX "fki_Dish_to_category";
+
+CREATE INDEX "fki_Dish_to_category"
+  ON "Dishs"
+  USING btree
+  ("CAT_ID");
+
+--Рецепты
+CREATE TABLE "Recipes"
+(
+  "ID" serial NOT NULL,
+  "DISH_ID" integer, -- Блюдо
+  "ING_ID" integer, -- Ингредиент
+  "QTY" real,
+  CONSTRAINT "Recipes_pkey" PRIMARY KEY ("ID"),
+  CONSTRAINT "Recipes_to_dish" FOREIGN KEY ("DISH_ID")
+      REFERENCES "Dishs" ("DISH_ID") MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT "Recipes_to_ingredient" FOREIGN KEY ("ING_ID")
+      REFERENCES "Ingredients" ("ING_ID") MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Recipes"
+  OWNER TO postgres;
+COMMENT ON TABLE "Recipes"
+  IS 'Рецепты';
+COMMENT ON COLUMN "Recipes"."DISH_ID" IS 'Блюдо';
+COMMENT ON COLUMN "Recipes"."ING_ID" IS 'Ингредиент';
+
+
+-- Index: "fki_Recipes_to_dish"
+
+-- DROP INDEX "fki_Recipes_to_dish";
+
+CREATE INDEX "fki_Recipes_to_dish"
+  ON "Recipes"
+  USING btree
+  ("DISH_ID");
+
+-- Index: "fki_Recipes_to_ingredient"
+
+-- DROP INDEX "fki_Recipes_to_ingredient";
+
+CREATE INDEX "fki_Recipes_to_ingredient"
+  ON "Recipes"
+  USING btree
+  ("ING_ID");
+  
+--Склады
+CREATE TABLE "Warehouse"
+(
+  "ID" serial NOT NULL,
+  "ING_ID" integer, -- Ингредиент
+  "QTY" real, -- Количество
+  CONSTRAINT "Warehouse_pkey" PRIMARY KEY ("ID"),
+  CONSTRAINT "Warehouse_to_ingredient" FOREIGN KEY ("ING_ID")
+      REFERENCES "Ingredients" ("ING_ID") MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Warehouse"
+  OWNER TO postgres;
+COMMENT ON TABLE "Warehouse"
+  IS 'Склад';
+COMMENT ON COLUMN "Warehouse"."ING_ID" IS 'Ингредиент';
+COMMENT ON COLUMN "Warehouse"."QTY" IS 'Количество';
+
+
+-- Index: "fki_Warehouse_to_ingredient"
+
+-- DROP INDEX "fki_Warehouse_to_ingredient";
+
+CREATE INDEX "fki_Warehouse_to_ingredient"
+  ON "Warehouse"
+  USING btree
+  ("ING_ID");
+
+--Содержимое меню
+CREATE TABLE "Menu_contents"
+(
+  "ID" serial NOT NULL,
+  "MENU_ID" integer, -- Меню
+  "DISH_ID" integer, -- Блюдо
+  CONSTRAINT "Menu_contents_pkey" PRIMARY KEY ("ID"),
+  CONSTRAINT "Menu_contents_to_dish" FOREIGN KEY ("DISH_ID")
+      REFERENCES "Dishs" ("DISH_ID") MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT "Menu_contents_to_menu" FOREIGN KEY ("MENU_ID")
+      REFERENCES "Menu" ("MENU_ID") MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Menu_contents"
+  OWNER TO postgres;
+COMMENT ON TABLE "Menu_contents"
+  IS 'Содержимое меню';
+COMMENT ON COLUMN "Menu_contents"."MENU_ID" IS 'Меню';
+COMMENT ON COLUMN "Menu_contents"."DISH_ID" IS 'Блюдо';
+
+
+-- Index: "fki_Menu_contents_to_menu"
+
+-- DROP INDEX "fki_Menu_contents_to_menu";
+
+CREATE INDEX "fki_Menu_contents_to_menu"
+  ON "Menu_contents"
+  USING btree
+  ("MENU_ID");
+
+-- Index: "fki_Menu_items_to_dish"
+
+-- DROP INDEX "fki_Menu_items_to_dish";
+
+CREATE INDEX "fki_Menu_items_to_dish"
+  ON "Menu_contents"
+  USING btree
+  ("DISH_ID");
+
+--Заказы
+CREATE TABLE "Orders"
+(
+  "ORD_ID" serial NOT NULL,
+  "WORK_ID" integer, -- Официант
+  "TABLENO" character varying(50), -- Номер столика
+  "ORDER_DATE" date, -- Дата заказа
+  CONSTRAINT "Orders_pkey" PRIMARY KEY ("ORD_ID"),
+  CONSTRAINT "Orders_to_worker" FOREIGN KEY ("WORK_ID")
+      REFERENCES "Workers" ("WORK_ID") MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Orders"
+  OWNER TO postgres;
+COMMENT ON TABLE "Orders"
+  IS 'Заказы';
+COMMENT ON COLUMN "Orders"."WORK_ID" IS 'Официант';
+COMMENT ON COLUMN "Orders"."TABLENO" IS 'Номер столика';
+COMMENT ON COLUMN "Orders"."ORDER_DATE" IS 'Дата заказа';
+
+
+-- Index: "fki_Orders_to_worker"
+
+-- DROP INDEX "fki_Orders_to_worker";
+
+CREATE INDEX "fki_Orders_to_worker"
+  ON "Orders"
+  USING btree
+  ("WORK_ID");
+
+--Содержимое заказа
+CREATE TABLE "Order_contents"
+(
+  "ID" serial NOT NULL,
+  "ORD_ID" integer, -- Заказ
+  "DISH_ID" integer, -- Блюдо
+  "QTY" real, -- Количество
+  CONSTRAINT "Order_contents_pkey" PRIMARY KEY ("ID"),
+  CONSTRAINT "Order_contents_to_dish" FOREIGN KEY ("DISH_ID")
+      REFERENCES "Dishs" ("DISH_ID") MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT "Order_contents_to_order" FOREIGN KEY ("ORD_ID")
+      REFERENCES "Orders" ("ORD_ID") MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Order_contents"
+  OWNER TO postgres;
+COMMENT ON TABLE "Order_contents"
+  IS 'Состав заказа';
+COMMENT ON COLUMN "Order_contents"."ORD_ID" IS 'Заказ';
+COMMENT ON COLUMN "Order_contents"."DISH_ID" IS 'Блюдо';
+COMMENT ON COLUMN "Order_contents"."QTY" IS 'Количество';
+
+
+-- Index: "fki_Order_contents_to_dish"
+
+-- DROP INDEX "fki_Order_contents_to_dish";
+
+CREATE INDEX "fki_Order_contents_to_dish"
+  ON "Order_contents"
+  USING btree
+  ("DISH_ID");
+
+-- Index: "fki_Order_contents_to_order"
+
+-- DROP INDEX "fki_Order_contents_to_order";
+
+CREATE INDEX "fki_Order_contents_to_order"
+  ON "Order_contents"
+  USING btree
+  ("ORD_ID");
+
+--Готовые блюда
+CREATE TABLE "Cooked_dishs"
+(
+  "ID" serial NOT NULL,
+  "DISH_ID" integer, -- Блюдо
+  "WORK_ID" integer, -- Повар
+  "ORD_ID" integer, -- Заказ
+  "DATE" date, -- Дата
+  CONSTRAINT "Cooked_dishs_pkey" PRIMARY KEY ("ID"),
+  CONSTRAINT "Cooked_dish_to_dish" FOREIGN KEY ("DISH_ID")
+      REFERENCES "Dishs" ("DISH_ID") MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT "Cooked_dish_to_orders" FOREIGN KEY ("ORD_ID")
+      REFERENCES "Orders" ("ORD_ID") MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT "Cooked_dish_to_work" FOREIGN KEY ("WORK_ID")
+      REFERENCES "Workers" ("WORK_ID") MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Cooked_dishs"
+  OWNER TO postgres;
+COMMENT ON TABLE "Cooked_dishs"
+  IS 'Приготовленные блюда';
+COMMENT ON COLUMN "Cooked_dishs"."DISH_ID" IS 'Блюдо';
+COMMENT ON COLUMN "Cooked_dishs"."WORK_ID" IS 'Повар';
+COMMENT ON COLUMN "Cooked_dishs"."ORD_ID" IS 'Заказ';
+COMMENT ON COLUMN "Cooked_dishs"."DATE" IS 'Дата';
+
+
+-- Index: "fki_Cooked_dish_to_dish"
+
+-- DROP INDEX "fki_Cooked_dish_to_dish";
+
+CREATE INDEX "fki_Cooked_dish_to_dish"
+  ON "Cooked_dishs"
+  USING btree
+  ("DISH_ID");
+
+-- Index: "fki_Cooked_dish_to_orders"
+
+-- DROP INDEX "fki_Cooked_dish_to_orders";
+
+CREATE INDEX "fki_Cooked_dish_to_orders"
+  ON "Cooked_dishs"
+  USING btree
+  ("ORD_ID");
+
+-- Index: "fki_Cooked_dish_to_work"
+
+-- DROP INDEX "fki_Cooked_dish_to_work";
+
+CREATE INDEX "fki_Cooked_dish_to_work"
+  ON "Cooked_dishs"
+  USING btree
+  ("WORK_ID");
+
+  
